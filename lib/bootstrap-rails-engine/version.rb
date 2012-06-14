b4da033c8bea2538d6d76bdcae639c19779ab158
Yen-Ju Chen <yjchenx@gmail.com>
2012-06-14 14:32:29 +0800

    update to bootstrap 2.0.4

--- lib/bootstrap-rails-engine/version.rb
+++ lib/bootstrap-rails-engine/version.rb
@@ -1,6 +1,6 @@
 module BootstrapRailsEngine
   module Rails
-    VERSION = "0.9.1"
-    TWITTER_BOOTSTRAP = "2.0.3"
+    VERSION = "0.9.2"
+    TWITTER_BOOTSTRAP = "2.0.4"
   end
 end
--- vendor/assets/javascripts/bootstrap/bootstrap-datepicker.js
+++ vendor/assets/javascripts/bootstrap/bootstrap-datepicker.js
@@ -47,6 +47,14 @@
 			});
 		} else {
 			if (this.component){
+				// For components that are not readonly, allow keyboard nav
+				this.element.find('input').on({
+					focus: $.proxy(this.show, this),
+					blur: $.proxy(this._hide, this),
+					keyup: $.proxy(this.update, this),
+					keydown: $.proxy(this.keydown, this)
+				});
+
 				this.component.on('click', $.proxy(this.show, this));
 				var element = this.element.find('input');
 				element.on({
@@ -194,7 +202,7 @@
 
 		update: function(){
 			this.date = DPGlobal.parseDate(
-				this.isInput ? this.element.prop('value') : this.element.data('date'),
+				this.isInput ? this.element.prop('value') : this.element.data('date') || this.element.find('input').prop('value'),
 				this.format, this.language
 			);
 			if (this.date < this.startDate) {
@@ -368,6 +376,7 @@
 						break;
 					case 'span':
 						if (!target.is('.disabled')) {
+							this.viewDate.setDate(1);
 							if (target.is('.month')) {
 								var month = target.parent().find('span').index(target);
 								this.viewDate.setMonth(month);
@@ -645,12 +654,29 @@
 			}
 			var parts = date ? date.match(this.nonpunctuation) : [],
 				date = new Date(),
-				val, filtered;
+				parsed = {},
+				setters_order = ['yyyy', 'yy', 'M', 'MM', 'm', 'mm', 'd', 'dd'],
+				setters_map = {
+					yyyy: function(d,v){ return d.setFullYear(v); },
+					yy: function(d,v){ return d.setFullYear(2000+v); },
+					m: function(d,v){
+						v -= 1;
+						d.setMonth(v);
+						while (d.getMonth() != v)
+							d.setDate(d.getDate()-1);
+						return d;
+					},
+					d: function(d,v){ return d.setDate(v); }
+				},
+				val, filtered, part;
+			setters_map['M'] = setters_map['MM'] = setters_map['mm'] = setters_map['m'];
+			setters_map['dd'] = setters_map['d'];
 			date = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
 			if (parts.length == format.parts.length) {
 				for (var i=0, cnt = format.parts.length; i < cnt; i++) {
 					val = parseInt(parts[i], 10)||1;
-					switch(format.parts[i]) {
+					part = format.parts[i];
+					switch(part) {
 						case 'MM':
 							filtered = $(dates[language].months).filter(function(){
 								var m = this.slice(0, parts[i].length),
@@ -668,24 +694,12 @@
 							val = $.inArray(filtered[0], dates[language].monthsShort) + 1;
 							break;
 					}
-					switch(format.parts[i]) {
-						case 'dd':
-						case 'd':
-							date.setDate(val);
-							break;
-						case 'mm':
-						case 'm':
-						case 'MM':
-						case 'M':
-							date.setMonth(val - 1);
-							break;
-						case 'yy':
-							date.setFullYear(2000 + val);
-							break;
-						case 'yyyy':
-							date.setFullYear(val);
-							break;
-					}
+					parsed[part] = val;
+				}
+				for (var i=0, s; i<setters_order.length; i++){
+					s = setters_order[i];
+					if (s in parsed)
+						setters_map[s](date, parsed[s])
 				}
 			}
 			return date;
